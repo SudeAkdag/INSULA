be0ab68b4c1f94a01e6c0873c14371f59c2272a2
Beyzanurguc <beyzanurguc06@gmail.com>
2026-02-10 23:11:07 +0300

    firebase kurulumu yapıldı

--- insula/lib/main.dart
+++ insula/lib/main.dart
@@ -1,8 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
 import 'package:insula/presentation/screens/main_screen.dart';
 import 'core/theme/app_theme.dart'; // Temayı içe aktar
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const InsulaApp());
 }
 
@@ -14,10 +23,8 @@ class InsulaApp extends StatelessWidget {
     return MaterialApp(
       title: 'Insula',
       debugShowCheckedModeBanner: false,
-      // Oluşturduğun merkezi temayı buraya bağlıyoruz
-      theme: AppTheme.lightTheme, 
-      // Uygulama açıldığında direkt Beslenme Takibi ekranı gelsin
-      home: const MainScreen(), 
+      theme: AppTheme.lightTheme,
+      home: const MainScreen(),
     );
   }
 }
